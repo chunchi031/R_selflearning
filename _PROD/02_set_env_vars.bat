3f463b3034405fff4fac36a5d374e253ed99f4fb
chunchi031 <33306302+chunchi031@users.noreply.github.com>
2023-10-10 20:30:56 +0800

    Update 02_set_env_vars.bat
    my setting

--- _PROD/02_set_env_vars.bat
+++ _PROD/02_set_env_vars.bat
@@ -4,21 +4,21 @@
 ::# User Environmental Variables
 ::# Check in: Computer Properties -> Advanced System Settings -> Environmental Variables -> User Variables
 
-setx PATH_DSS_Repo "C:\Users\fxtrams\Documents\000_TradingRepo"
+setx PATH_DSS_Repo "%USERPROFILE%\Documents\GitHub"
 
-setx PATH_T1 "C:\Program Files (x86)\FxPro - Terminal1\MQL4\Files"
-setx PATH_T1_E "C:\Program Files (x86)\FxPro - Terminal1\MQL4\Experts"
-setx PATH_T1_I "C:\Program Files (x86)\FxPro - Terminal1\MQL4\Include"
+setx PATH_T1 "C:\Program Files (x86)\Axi Terminal1\MQL4\Files"
+setx PATH_T1_E "C:\Program Files (x86)\Axi Terminal1\MQL4\Experts"
+setx PATH_T1_I "C:\Program Files (x86)\Axi Terminal1\MQL4\Include"
 
-setx PATH_T2 "C:\Program Files (x86)\FxPro - Terminal2\MQL4\Files"
-setx PATH_T2_E "C:\Program Files (x86)\FxPro - Terminal2\MQL4\Experts"
-setx PATH_T2_I "C:\Program Files (x86)\FxPro - Terminal2\MQL4\Include"
+setx PATH_T2 "C:\Program Files (x86)\Axi Terminal1\MQL4\Files"
+setx PATH_T2_E "C:\Program Files (x86)\Axi Terminal1\MQL4\Experts"
+setx PATH_T2_I "C:\Program Files (x86)\Axi Terminal1\MQL4\Include"
 
-setx PATH_T3 "C:\Program Files (x86)\FxPro - Terminal3\MQL4\Files"
-setx PATH_T3_E "C:\Program Files (x86)\FxPro - Terminal3\MQL4\Experts"
-setx PATH_T3_I "C:\Program Files (x86)\FxPro - Terminal3\MQL4\Include"
+setx PATH_T3 "C:\Program Files (x86)\Axi Terminal1\MQL4\Files"
+setx PATH_T3_E "C:\Program Files (x86)\Axi Terminal1\MQL4\Experts"
+setx PATH_T3_I "C:\Program Files (x86)\Axi Terminal1\MQL4\Include"
 
-setx PATH_T4 "C:\Program Files (x86)\FxPro - Terminal4\MQL4\Files"
-setx PATH_T4_E "C:\Program Files (x86)\FxPro - Terminal4\MQL4\Experts"
-setx PATH_T4_I "C:\Program Files (x86)\FxPro - Terminal4\MQL4\Include"
+setx PATH_T4 "C:\Program Files (x86)\Axi Terminal1\MQL4\Files"
+setx PATH_T4_E "C:\Program Files (x86)\Axi Terminal1\MQL4\Experts"
+setx PATH_T4_I "C:\Program Files (x86)\Axi Terminal1\MQL4\Include"
 
